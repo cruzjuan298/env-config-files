e3ba9fa469acff20fd8bfc13a39f1a5f82adf389
Juan Cruz <cruzjuan298@gmail.com>
2025-11-25 22:40:35 -0500

    update readme and installation script

--- README.md
+++ README.md
@@ -1,17 +1,30 @@
-## My Personal Neovim Configuration files
+## My Personal Environment Configuration files
 
-The config files are both seperated into a lua folder in my /.config/nvim path. Generally, this makes it more organized for me to edit and add plugins.
-If you'd like the same setup, run the following for both files:
-````sh
-cp <your clone directory>/file.lua /.config.nvim/lua/file.lua
-````
-## Walkthroughh
+The config files are seperaed into two sections. The installation script handles all most of the work when trying to copy the files in the correct directory. 
+
+## Neovim
+The neovim folder is organized within a lua folder. Generally, this makes it much more easier for me to edit and add plugins. It possible to remove all the contents in this 
+file and just have all files within the nvim directory but things start to get messy when you have dozen of different config files for different plugins.
+
+## Neovim - Walkthroughh
 The first portion of the plugin.lua file sets up lazy.nvim. In that table is where all of my plugins reside. From top to bottom they include:
 - colorscheme
 - treesitter
 - portable package manager
 - lsp's
 - file explorers (newtr is set by default but oil.nvim is included for easy setup)
+- harpoon
+- flash
+- lazygit
+- supermaven
 
 I decided to use mason for my packagae manager (for now). To install lsp's, run the ```:MasonInstall {lsp name}``` or the ```:Mason``` command to see the menu of avilable lsp's.
-Lsp's will automatically be configured because of the function in the mason-lsp-config.nvim table.
+Lsp's will automatically be configured because of the function in the mason-lsp-config.nvim table. **NOTE** If you aren't able to find the appriopate lsp for your use case,
+you can always write you own or find it in the collections of lsp config in nvim-lspconfig.
+
+## Tmux
+The tmux folder contains only the necessary tmux config file. Like mentioned before, the installation script handles installing tpm for the tmux plugins in the config file.
+
+## Tmux - Walkthroughh
+The first poritons of the file is focused on changing the default tmux key-bindings. Then, the colorscheme follows and lastly the plugins are included. The comments
+provide specification as what exactly is in each section.
--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash -e 
+#!/usr/bin/env bash 
+set -e
 
-TMUX_SRC_DIR="./tmux"
-NVIM_SRC_DIR="./nvim" 
+TMUX_SRC_DIR="./tmuxConfigFiles"
+NVIM_SRC_DIR="./neovimConfigFiles" 
 
 TMUX_TARGET="$HOME/.tmux.conf"
 NVIM_TARGET="$HOME/.config/nvim"
@@ -12,28 +13,28 @@ check_installed() {
 
 prompt() {
     read -rp "$1 (y/n): " answer 
-    [[ "$answer" == "y" || "$answer" == "n" ]] 
+    [[ "$answer" == "y" || "$answer" == "Y" ]] 
 }
 
 install_pck() {
     local pkg="$1"
 
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        if command -v apt >/dev/null 2>$1; then
+        if command -v apt >/dev/null 2>&1; then
             sudo apt update && sudo apt install -y "$pkg"
-        elif command -v packman >/dev/null 2>$1; then
-            sudo packman -$ "$pkg"
-        elif command -v dnf >/dev/null 2>$1; then
+        elif command -v pacman >/dev/null 2>&1; then
+            sudo pacman -S --noconfirm "$pkg"
+        elif command -v dnf >/dev/null 2>&1; then
             sudo dnf install -y "$pkg"
         fi
     elif [[ "$OSTYPE" == "darwin"* ]]; then
-        if command -v brew >/dev/null 2>$1; then
-            brew install -y "$pkg"
+        if command -v brew >/dev/null 2>&1; then
+            brew install "$pkg"
         else
             echo "Homebrew is not installed"
         fi
     else
-        echo "Unsupported system. Install "$pkg" manually"
+        echo "Unsupported system. Install $pkg manually"
     fi
 }
 
@@ -41,7 +42,7 @@ copy_config() {
     local src="$1"
     local target="$2"
 
-    if [[ -e "target" ]]; then
+    if [[ -e "$target" ]]; then
         echo "Existing config found: $target"
         if prompt "Overwrite current config files?"; then
             rm -rf "$target"
@@ -62,7 +63,7 @@ if ! check_installed nvim; then
     
     echo "Neovim not found"
 
-    if prompt "Install tmux?"; then
+    if prompt "Install neovim?"; then
         install_pck neovim
     else
         echo "Skipping neovim installation"
@@ -75,8 +76,14 @@ if ! check_installed tmux; then
     
     echo "Tmux not found"
 
-    if prompt "Install tmux?"; then
-        install_pck tmux
+    if prompt "Install tmux?"; then     
+        install_pck tmux 
+        echo "Tmux has been installed. Now installing tpm (Tmux Package Manager)"
+        mkdir -p ~/.tmux/plugins
+        if [[ ! -d ~/.tmux/plugins/tpm ]]; then
+            git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 
+        fi
+        echo "TPM has been installed"
     else
         echo "Skipping tmux installation"
     fi
@@ -87,11 +94,7 @@ fi
 echo ""
 echo "Installing config files"
 
-
-echo ""
-echo "Installing config files"
-
-copy_config "$TMUX_SRC_DIR/.tmux/conf" "$TMUX_TARGET"
+copy_config "$TMUX_SRC_DIR/.tmux.conf" "$TMUX_TARGET"
 copy_config "$NVIM_SRC_DIR" "$NVIM_TARGET"
 
 echo ""
